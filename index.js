c29b9c2501ce3f76a51d57d80c1dab9cb695c79f
Gurdip Singh <singh.gurdip24@gmail.com>
2020-12-06 21:46:43 -0500

    handled not found error

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ const express = require('express');
 const { request } = require('http');
 const path = require('path');
 const members =require('./Members');
+const logger = require('./middleware/logger');
+
 const app = express();
 
+// init middleware
+app.use(logger);
+
 app.get('/api/members',(req,res) => res.json(members));
+
+app.get('/api/members/:id',(req,res) => {
+
+    const found = members.some(member => member.id === req.params.id);
+
+    if(found){
+        res.json(members.filter(member => member.id === parseInt(req.params.id)));   
+    } else {
+        res.status(400).json({msg:`Member id ${req.params.id} not found`});
+    }
+
+});
 
 app.use(express.static(path.join(__dirname,'public')));
 
