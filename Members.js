58a1d1fc24f202ff1af635422a0aad97150df432
Gurdip Singh <singh.gurdip24@gmail.com>
2020-12-06 19:39:58 -0500

    first get end point

---
+++ Members.js
@@ -0,0 +1,34 @@
+const members = [
+    {
+		"id": 1,
+		"name": "Jerome",
+		"email": "egestas.urna.justo@Nullaaliquet.org",
+		"status": "Active"
+	},
+	{
+		"id": 2,
+		"name": "Caesar",
+		"email": "eleifend.egestas@atvelit.net",
+		"status": "Inactive"
+	},
+	{
+		"id": 3,
+		"name": "Gray",
+		"email": "risus@Donecluctusaliquet.com",
+		"status": "Inactive"
+	},
+	{
+		"id": 4,
+		"name": "Oliver",
+		"email": "risus@elitafeugiat.ca",
+		"status": "Inactive"
+	},
+	{
+		"id": 5,
+		"name": "Dexter",
+		"email": "risus.Nunc@facilisismagna.co.uk",
+		"status": "Active"
+	}
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const { request } = require('http');
 const path = require('path');
-
+const members =require('./Members');
 const app = express();
 
-app.use(express.static(path.join(__dirname,'public')))
+app.get('/api/members',(req,res) => res.json(members));
+
+app.use(express.static(path.join(__dirname,'public')));
 
 const PORT = process.env.PORT || 5000;
 
